740f8bfbbf88a9da69a24ab4240c90bce4e3e3a3
Dieter Niklaus <dieter.niklaus@gmx.net>
2019-02-01 12:13:54 +0100

    first adaptation for LoRa, missing Assets, LoRaWan, and more components...

--- src/main.cpp
+++ src/main.cpp
@@ -23,12 +23,31 @@
 #include <AppDebug.h>
 #include <ProductDebug.h>
 #include <RamUtils.h>
+#include <Assets.h>
+#include <MyDeviceSerialNrAdapter.h>
+#include <DetectorFakePersDataMemory.h>
+#include <Battery.h>
+#include <MyBatteryAdapter.h>
+#include <LoraWanAbp.hpp>
+#include <LoRaWanDriver.hpp>
+#include <MyLoRaWanConfigAdapter.h>
+#include <LoraWanPriorityQueue.hpp>
+#include <pb_encode.h>
+#include <pb_decode.h>
+#include <SerialCommand.h>
+
+/* This is the buffer where we will store our message. */
+bool setMessageOnce = true;
+LoRaWanDriver* m_LoraWanInterface;
+LoraWanPriorityQueue* m_LoraWanPriorityQueue;
 
 #ifndef BUILTIN_LED
 #define BUILTIN_LED 13
 #endif
 
 SerialCommand* sCmd = 0;
+Assets* assets = 0;
+Battery* battery = 0;
 
 void setup()
 {
@@ -36,6 +55,38 @@ void setup()
   digitalWrite(BUILTIN_LED, 0);
 
   setupProdDebugEnv();
+
+  //-----------------------------------------------------------------------------
+  // Assets (inventory and persistent data)
+  //-----------------------------------------------------------------------------
+  assets = new Assets(new MyDeviceSerialNrAdapter(), new DetectorFakePersDataMemory());
+
+  //-----------------------------------------------------------------------------
+  // Battery Voltage Surveillance
+  //-----------------------------------------------------------------------------
+  BatteryThresholdConfig battCfg = { 3.6, // BATT_WARN_THRSHD [V]
+                                     3.4, // BATT_STOP_THRSHD [V]
+                                     3.2, // BATT_SHUT_THRSHD [V]
+                                     0.1  // BATT_HYST        [V]
+                                    };
+  battery = new Battery(new MyBatteryAdapter(), battCfg);
+
+  //-----------------------------------------------------------------------------
+  // Sensors
+  //-----------------------------------------------------------------------------
+  pmProcess = new PM_Process(&Serial1, new MyPM_ProcessAdapter());
+  pmProcess->init(9600);
+  dhtProcess = new DHT_Process(new MyDHT_ProcessAdapter());
+
+  //-----------------------------------------------------------------------------
+  // LoRaWan
+  //-----------------------------------------------------------------------------
+  m_LoraWanInterface = new LoraWanAbp(new MyLoRaWanConfigAdapter(assets));
+  m_LoraWanPriorityQueue = new LoraWanPriorityQueue(m_LoraWanInterface);
+  // m_MeasurementFacade = new MeasurementFacade(m_LoraWanPriorityQueue);
+  // m_SystemStatusFacade = new SystemStatusFacade(m_LoraWanPriorityQueue);
+  m_LoraWanPriorityQueue->setUpdateCycleHighPriorityPerdioc(2);
+  m_LoraWanPriorityQueue->start();
 }
 
 void loop()
@@ -45,4 +96,6 @@ void loop()
     sCmd->readSerial();     // process serial commands
   }
   yield();                  // process Timers
+  m_LoraWanInterface->loopOnce();
+  m_LoraWanPriorityQueue->update();
 }
